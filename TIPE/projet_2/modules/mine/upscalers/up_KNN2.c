379e1e8619059cd868cb8dacd1af8445440e2144
yan-ox <yanuxdyno@gmail.com>
2026-09-14 20:04:40 +0200

    KNN done but needs testing, next to do: full apply

--- TIPE/projet_2/modules/mine/upscalers/up_KNN2.c
+++ TIPE/projet_2/modules/mine/upscalers/up_KNN2.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <assert.h>
 
-void KNN(img* base, img* out, size_t i, int k);
+void KNN(img* base, img* out, size_t i, int k); //Needs testing
 int* create_nearest(int max_rad, int* coord, int k); //works
 
 void print_mult_coord(int* coords, int n){
@@ -17,6 +17,10 @@ void print_mult_coord(int* coords, int n){
 }
 
 int* create_nearest(int max_rad, int* coord, int k){
+    /*Creates the array of the k-closest pixels 
+    of the given point.
+    max_rad is here to stop the search if it goes to far away from the orginal point
+    */
     int* nearest = malloc(2 * k * sizeof(int));
     nearest[0] = coord[0];
     nearest[k] = coord[1];
@@ -55,15 +59,25 @@ int* create_nearest(int max_rad, int* coord, int k){
     return nearest;
 }
 
-/*
+
 void KNN(img* base, img* out, size_t i, int k){
     float coef = out->width / (float)base->width;
-    int* coordo = {0, 1};
-    coor(i, out->width, coordo);
-    int* coordb = {(int)(coordo[0] / coef), (int)(coordo[1] / coef)};
-
+    int* coordb = malloc(2 * sizeof(int));
+    coor(i, out->width, coordb);
+    coordb[0] = (int)(coordb[0] / coef);
+    coordb[1] = (int)(coordb[1] / coef);
+    int* nearest = create_nearest(base->width / 2, coordb, k);
+    float RGB[3] = {0, 0, 0};
+    for(int j = 0; j < k; j++){
+        RGB[0] += base->tab[ind(nearest[j], nearest[j + k], base->width)];
+        RGB[1] += base->tab[ind(nearest[j], nearest[j + k], base->width) + 1];
+        RGB[2] += base->tab[ind(nearest[j], nearest[j + k], base->width) + 2];
+    }
+    base->tab[i] = RGB[0] / k;
+    base->tab[i + 1] = RGB[1] / k;
+    base->tab[i + 2] = RGB[2] / k;
 }
-*/
+
 
 
 
@@ -72,7 +86,7 @@ void main(){
     img* base = img_open("smalltest.png");
     int test_bcoord[2] = {2, 2};
     int* test_coord = create_nearest(50, test_bcoord, 25);
-    print_mult_coord(test_coord, 25);
+    //print_mult_coord(test_coord, 25);
     /*img* upscaled = expand(base, 1.2 * base->width);
     printf("upscale\n");
     printf("bwidth * bheigth: %d\n", base->height * base->width);
