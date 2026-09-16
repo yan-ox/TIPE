d1ca4882763d1c4c03e18a114411780a1b5c6a3e
yan-ox <yanuxdyno@gmail.com>
2026-09-16 17:07:19 +0200

    Fixed main problems and commented everything, there are still problems with the render

--- TIPE/projet_2/modules/mine/upscalers/up_KNN2.c
+++ TIPE/projet_2/modules/mine/upscalers/up_KNN2.c
@@ -8,12 +8,16 @@
 #include <math.h>
 
 void KNN(img* base, img* out, size_t i, int k, float coef); //Needs testing
-int* create_nearest(int max_rad, int* coord, int k); //works
-void full_apply(img* base, img* out, int k); //WIP
+int* create_nearest(int width, int height, int max_rad, int* coord, int k); //works
+void full_apply(img* base, img* out, int k, float coef); //WIP
 bool float_equal(float a, float b, float e);
 
 bool float_equal(float a, float b, float e){
     return fabs(a - b) < e;
+}
+
+void print_pixel(img* im, size_t i){
+    printf("RGB: (%d, %d, %d)\n", im->tab[i], im->tab[i+1], im->tab[i+2]);
 }
 
 void print_mult_coord(int* coords, int n){
@@ -24,7 +28,7 @@ void print_mult_coord(int* coords, int n){
     }
 }
 
-int* create_nearest(int max_rad, int* coord, int k){
+int* create_nearest(int width, int height, int max_rad, int* coord, int k){
     /*Creates the array of the k-closest pixels 
     of the given point.
     max_rad is here to stop the search if it goes to far away from the orginal point
@@ -46,7 +50,7 @@ int* create_nearest(int max_rad, int* coord, int k){
                     int nx = (coord[0] - r) + j;
                     int ny = (coord[0] - r) + p;
                     //Verifies that the pixels are within the image
-                    if(nx >= 0 && ny >= 0){
+                    if(nx >= 0 && ny >= 0 && nx < width && ny < height){
                         nearest[i] = nx;
                         nearest[k + i] = ny;
                         i ++;
@@ -75,46 +79,76 @@ void KNN(img* base, img* out, size_t i, int k, float coef){
     coor(i, out->width, coordb);
     coordb[0] = (int)(coordb[0] / coef);
     coordb[1] = (int)(coordb[1] / coef);
+    //printf("x:%d, y:%d\n", coordb[0], coordb[1]);
     //Finds the closest pixels
-    int* nearest = create_nearest(base->width / 2, coordb, k);
+    int* nearest = create_nearest(base->width, base->height, base->width / 2, coordb, k);
+    //print_mult_coord(nearest, k);
     //Takes the average of the pixels
     float RGB[3] = {0, 0, 0};
     for(int j = 0; j < k; j++){
-        RGB[0] += base->tab[ind(nearest[j], nearest[j + k], base->width)];
-        RGB[1] += base->tab[ind(nearest[j], nearest[j + k], base->width) + 1];
-        RGB[2] += base->tab[ind(nearest[j], nearest[j + k], base->width) + 2];
+        RGB[0] += base->tab[ind(nearest[j], nearest[j + k], base->width) * out->channels];
+        RGB[1] += base->tab[ind(nearest[j], nearest[j + k], base->width) * out->channels + 1];
+        RGB[2] += base->tab[ind(nearest[j], nearest[j + k], base->width) * out->channels + 2];
     }
-    base->tab[i] = RGB[0] / k;
-    base->tab[i + 1] = RGB[1] / k;
-    base->tab[i + 2] = RGB[2] / k;
+    //And inputs the average into the image
+    out->tab[i * out->channels] = RGB[0] / k;
+    out->tab[i * out->channels + 1] = RGB[1] / k;
+    out->tab[i * out->channels + 2] = RGB[2] / k;
     free(coordb);
     free(nearest);
 }
 
-void full_apply(img* base, img* out, int k){
+void full_apply(img* base, img* out, int k, float coef){
+    /*Applies KNN to a an empty image (out)
+    based of the pixels ouf an other one (base)
+    */
     //
-    float coef = out->width / (float)base->width;
+    //Needed variables intialisation
     size_t base_size = base->width * base->height;
     size_t otp_size = out->width * out->height;
+    //Will be used for the unknown pixels
     int* coordo = malloc(2 * sizeof(int));
-    int* coordb = malloc(2 * sizeof(int));
-    float eps = (float)1/4;
+    //Will be used for the known pixels
+    int* coordbo = malloc(2 * sizeof(int));
     //
-    for (size_t io = 0; io < otp_size; io ++){
+    for (size_t ib = 0; ib < base_size; ib ++){
         //Calculates the coordinates
-        coor(io, out->width, coordo);
-        coordb[0] = (int)(coordo[0] / coef);
-        coordb[1] = (int)(coordo[1] / coef);
-        size_t ib = ind(coordb[0], coordb[1], base->width); 
-        if (float_equal((float)coordb[0], coordo[0] / coef, eps) && float_equal((float)coordb[1], coordo[1] / coef, eps)){
-            printf("(%d, %d)\n", coordb[0], coordb[1]);
+        //Start-filling coordinates
+        //
+        coor(ib - 1, base->width, coordo);
+        coordo[0] = (int)(coordo[0] * coef);
+        coordo[1] = (int)(coordo[1] * coef);
+        size_t io = ind(coordo[0], coordo[1], out->width) + 1;
+        //
+        //printf("io:(%d, %d)\n", coordb[0], coordb[1]);
+        //
+        //Next known pixel
+        coor(ib, base->width, coordbo);
+        coordbo[0] = (int)(coordbo[0] * coef);
+        coordbo[1] = (int)(coordbo[1] * coef);
+        size_t ibo = ind(coordbo[0], coordbo[1], out->width); 
+        //
+        //printf("ibo:(%d, %d)\n", coordbo[0], coordbo[1]);
+        //printf("pre boucle io:%d, ibo: %d\n", io, ibo);
+        //
+        //Fills pixels until we step on the known pixel's coordinates
+        while(io < ibo && io < otp_size){
+            //printf("io:%d\n", io);
+            KNN(base, out, io, k, coef);
+            //print_pixel(out, io);
+            io ++;
+        }
+        //Then fills the known pixel
+        if(ibo < otp_size){
             for (int j = 0; j < base->channels; j ++){
                 //Fills the known pixels to designated places
-                out->tab[io * out->channels + j] = base->tab[io * out->channels + j];
+                out->tab[ibo * out->channels + j] = base->tab[ib * out->channels + j];
             }
-        }else{
-            KNN(base, out, io, k, coef);
         }
+        //assert(ib!=101250);
+        //
+        free(coordo);
+        free(coordbo);
     }
 }
 
@@ -122,20 +156,22 @@ void full_apply(img* base, img* out, int k){
 
 
 void main(){
-    img* base = img_open("smalltest.png");
+    img* base = img_open("Gaelle.png");
     int test_bcoord[2] = {2, 2};
-    int* test_coord = create_nearest(50, test_bcoord, 25);
+    //int* test_coord = create_nearest(50, test_bcoord, 25);
     //print_mult_coord(test_coord, 25);
-    img* upscaled = make_expand(base, 1.2 * base->width);
+    float coef = 2.0;
+    img* upscaled = make_expand(base, coef * base->width);
     printf("expand\n");
+    printf("size: %d", upscaled->width * upscaled->height);
     //printf("bwidth * bheigth: %d\n", base->height * base->width);
     //printf("RGBb: %d, %d, %d\n", upscaled->tab[30801], upscaled->tab[30801 + 1], upscaled->tab[30801 + 2]);
-    full_apply(base, upscaled, 9);
+    full_apply(base, upscaled, 9, coef);
     printf("KNN\n");
     savesupr_img(upscaled, "test_up.png");
     printf("save\n");
     free_img(upscaled);
-    free(test_coord);
+    //free(test_coord);
     free_img(base);
     printf("freed\n");
 }
